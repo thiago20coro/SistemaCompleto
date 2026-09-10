edd088df9d1f1d51e5e839313b7b76e102532c88
thiago20coro <thiago20coro@gmail.com>
2026-09-10 12:22:30 -0300

    mude

--- sistema-completo/api/index.js
+++ sistema-completo/api/index.js
@@ -1,3 +1,8 @@
 import app from '../src/components/server.js'
 
-export default app
+export default function handler(request, response) {
+	if (request.url?.startsWith('/api')) {
+		request.url = request.url.slice(4) || '/'
+	}
+	return app(request, response)
+}
